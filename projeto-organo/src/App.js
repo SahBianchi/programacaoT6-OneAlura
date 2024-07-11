4fe500256273360867a0cb42a3e767c163703f77
SahBianchi <samantha.sbianchi@gmail.com>
2024-07-11 01:28:49 -0300

    feat: creating the component Rodape and refactor: components: CampoTexto, Colaborador, Formulario, ListaSuspensa, Time

--- projeto-organo/src/App.js
+++ projeto-organo/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
+import Rodape from './componentes/Rodape';
 
 function App() {
  
@@ -46,7 +47,6 @@ function App() {
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
   }
 
@@ -54,13 +54,15 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      {times.map(time => <Time 
+      {times.map(time => 
+      <Time 
         key={time.nome}
         nome={time.nome}
         corPrimaria={time.corPrimaria}
         corSecundaria={time.corSecundaria}
         colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
         />)}
+      <Rodape />
       
     </div>
   );
